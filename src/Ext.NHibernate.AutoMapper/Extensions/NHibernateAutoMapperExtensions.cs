8237fbfd5e1fa9da76c7873ab1a1ee60a197a92b
patrinhani-ciandt <patrinhani@ciandt.com>
2014-10-28 07:24:47 -0500

    Add new Ext project for NHibernate AutoMapper

---
+++ src/Ext.NHibernate.AutoMapper/Extensions/NHibernateAutoMapperExtensions.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using NHibernate;
+using System;
+using System.Linq.Expressions;
+
+namespace Bohrium.Ext.NHibernate.AutoMapper.Extensions
+{
+    /// <summary>
+    /// NHibernate Extension Methods for AutoMapper
+    /// </summary>
+    public static class NHibernateAutoMapperExtensions
+    {
+        /// <summary>
+        /// Ignore the map for a property that is not initialized by the lazy load.
+        /// </summary>
+        /// <typeparam name="TPersistent">Persistent Object Type</typeparam>
+        /// <param name="cfg">AutoMapper Member Configuration</param>
+        /// <param name="memberToBeChecked">Lambda expression to access the property to be checked</param>
+        public static void IgnoreIfNotInitialized<TPersistent>(this IMemberConfigurationExpression<TPersistent> cfg, Expression<Func<TPersistent, object>> memberToBeChecked)
+        {
+            cfg.Condition(src => NHibernateUtil.IsInitialized(memberToBeChecked.Compile()(src)));
+        }
+    }
+}
